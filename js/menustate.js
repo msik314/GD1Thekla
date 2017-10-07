15401543f483cc4d3da2025ea82ca0e959364dbb
upscalerat <heather.siobhans@gmail.com>
2017-10-07 18:02:31 -0400

    Menu state

---
+++ js/menustate.js
@@ -0,0 +1,19 @@
+//Constructor
+let menuState=function(){
+	
+}
+
+menuState.prototype.preload=function(){
+	
+}
+
+menuState.prototype.create=function(){
+	//Just the middle of the screen for now, until theres an actual graphic
+	startButton=game.add.sprite(375,667,"startButton");
+	startButton.inputEnabled=true;
+	startButton.events.onInputDown.add(transition, this);
+}
+
+menuState.prototype.transition=function(){
+	game.state.start("levelonestate");
+}
