2e73a8062536c398e41ae2c8838e3585126e9e0b
DavidTheGinger <davidprc3@gmail.com>
2017-10-17 22:39:15 -0400

    fixed arrow overlap issue

--- js/levelonestate.js
+++ js/levelonestate.js
@@ -38,6 +38,10 @@ leveloneState.prototype.preload = function(){
 	game.load.spritesheet("wind", "assets/3dStuff/background/spritesheet_wind_v1.png",1334, 750,60);
 	game.load.audio("songOne","assets/tk30s.ogg");
 	
+	
+	game.load.audio("correct","assets/tkBellHigh.ogg");
+	game.load.audio("barMade","assets/tkBellLow.ogg");
+	
 	//create background
 	this.water = game.add.sprite(0,0,'water');
 	this.water.animations.add('normal');
@@ -57,6 +61,8 @@ leveloneState.prototype.preload = function(){
 
 leveloneState.prototype.create = function(){
 	//reset variables on reload
+	this.numBars = 0;
+	this.barsHit = 0;
 	this.score = 0;
 	this.swiping = false;
 	this.startX = 0;
@@ -78,11 +84,12 @@ leveloneState.prototype.create = function(){
 	this.musicPlaying = false;
 	
 	//some constants for quick global tweaks
-	this.arrowSpeed = -500;
+	this.arrowSpeed = -800;
 	this.bufferLength = 3.2;
 	this.spawnBuffer = .8;
 	this.barSpawnY = -200;
 	this.barSpeed = (game.world.height - 200 - this.barSpawnY)/this.spawnBuffer;
+	this.instrVol = 10;
 	
 	
 	
@@ -101,25 +108,33 @@ leveloneState.prototype.create = function(){
 	this.directionText = game.add.text(16, 50, 'Score: 0', { fontSize: '32px', fill: '#000000' });
 	music = game.add.audio('songOne');
 	leftSFX = game.add.audio('leftAud');
-	leftSFX._volume = 5;
+	leftSFX._volume = this.instrVol;
 	rightSFX = game.add.audio('rightAud');
-	rightSFX._volume = 5;
+	rightSFX._volume = this.instrVol;
 	upSFX = game.add.audio('upAud');
-	upSFX._volume = 5;
+	upSFX._volume = this.instrVol;
 	downSFX = game.add.audio('downAud');
-	downSFX._volume = 5;
+	downSFX._volume = this.instrVol;
+	
+	barMadeSFX = game.add.audio('barMade');
+	barMadeSFX._volume = this.instrVol;
+	
+	barCorrectSFX = game.add.audio('correct');
+	barCorrectSFX._volume = this.instrVol;
+	
 	//music.onDecoded.add(start, this);
 	//music.play();
 	
 	//create instructor;
 	this.instructor = game.add.sprite(0,0,"instructor");
-	this.instructor.animations.add('instructing', [0,1],2.1,true);
+	//this.instructor.animations.add('instructing', [0,1],2.1,true);
 	this.instructor.alpha = 0;
-	this.instructor.animations.play('instructing');
+	//this.instructor.animations.play('instructing');
 	
 	//create arrow group
 	this.arrows = game.add.group();
 	this.arrows.enableBody = true;
+	//this.arrows.body.onCollide.add(this.arrowStop, this);
 	
 	//create bar group
 	this.bars = game.add.group();
@@ -161,6 +176,7 @@ leveloneState.prototype.update = function(){
 	
 	//collide arrows with each other for stacking
 	game.physics.arcade.collide(this.arrows, this.arrows);
+	game.physics.arcade.overlap(this.arrows, this.arrows, this.arrowStop, null, this);
 	
 	
 	//SWIPE FUNCTIONALITY -- ONLY CHECK IF LOOKING FOR INPUT
@@ -216,6 +232,12 @@ leveloneState.prototype.update = function(){
 	
 }
 
+leveloneState.prototype.arrowStop = function(sprite1,sprite2){
+	sprite1.body.velocity.x = 0;
+	sprite2.body.velocity.x = 0;
+}
+
+
 //first instruction in a set
 leveloneState.prototype.firstInstruct = function(){
 	this.score++;
@@ -253,11 +275,13 @@ leveloneState.prototype.instruct = function(){
 
 leveloneState.prototype.sendBar = function(){
 	//instructor.
+	this.numBars++;
 	this.removeArrows();
 	this.score = this.instrTimes[this.instrIndex];
 	this.scoreText.text = 'Score: ' + this.score;
 	this.instructor.alpha = 0;
 	let bar = this.bars.create(0,this.barSpawnY, "bar");
+	barMadeSFX.play();
 	bar.body.velocity.y = this.barSpeed;
 	this.checkNext();
 }
@@ -358,6 +382,8 @@ leveloneState.prototype.swiped = function(){
 					if(this.currentDirections[this.currentDirectionsIndex] === 1){
 						if(temp.y < game.world.height){
 							this.scoreText.text = 'Score: correct';
+							this.barsHit++;
+							barCorrectSFX.play();
 							//this.removeBar();
 						}else{
 							this.scoreText.text = 'Score: TooSoon';
@@ -372,6 +398,8 @@ leveloneState.prototype.swiped = function(){
 					if(this.currentDirections[this.currentDirectionsIndex] === 3){
 						if(temp.y < game.world.height){
 							this.scoreText.text = 'Score: correct';
+							this.barsHit++;
+							barCorrectSFX.play();
 							//this.removeBar();
 						}else{
 							this.scoreText.text = 'Score: TooSoon';
@@ -389,6 +417,8 @@ leveloneState.prototype.swiped = function(){
 					if(this.currentDirections[this.currentDirectionsIndex] === 2){
 						if(temp.y < game.world.height){
 							this.scoreText.text = 'Score: correct';
+							this.barsHit++;
+							barCorrectSFX.play();
 							//this.removeBar();
 						}else{
 							this.scoreText.text = 'Score: TooSoon';
@@ -403,6 +433,8 @@ leveloneState.prototype.swiped = function(){
 					if(this.currentDirections[this.currentDirectionsIndex] === 0){
 						if(temp.y < game.world.height){
 							this.scoreText.text = 'Score: correct';
+							this.barsHit++;
+							barCorrectSFX.play();
 							//this.removeBar();
 						}else{
 							this.scoreText.text = 'Score: TooSoon';
